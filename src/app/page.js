04ea317d3eebe21af4669617606d3758fee14415
krish vaghasiya <kvaghasiya705@gmail.com>
2025-05-09 17:27:39 +0530

    Created two more section's

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Effortless from "@/components/homecomponents/effortlesssection";
 import Homeherobanner from "@/components/homecomponents/herobanner";
 import Makefinacedata from "@/components/homecomponents/makefinacedata";
+import Oldnewsection from "@/components/homecomponents/oldnewsection";
 import Smartinvestment from "@/components/homecomponents/smartinvestmentsection";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Makefinacedata />
       <Smartinvestment />
       <Effortless />
+      <Oldnewsection />
     </>
   );
 }
