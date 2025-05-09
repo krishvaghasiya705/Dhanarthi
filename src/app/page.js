8c39dd371d02092a58b85fa3e6e84e443779e10d
krish vaghasiya <kvaghasiya705@gmail.com>
2025-05-09 18:41:53 +0530

    Created old newsection, why choose section, step section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Analyze from "@/components/homecomponents/analyzesection";
 import Effortless from "@/components/homecomponents/effortlesssection";
 import Homeherobanner from "@/components/homecomponents/herobanner";
 import Makefinacedata from "@/components/homecomponents/makefinacedata";
 import Oldnewsection from "@/components/homecomponents/oldnewsection";
 import Smartinvestment from "@/components/homecomponents/smartinvestmentsection";
+import Whychoosesection from "@/components/homecomponents/whychoosesection";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <Smartinvestment />
       <Effortless />
       <Oldnewsection />
+      <Whychoosesection />
+      <Analyze />
     </>
   );
 }
