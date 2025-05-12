302b9824941a1742ca427ad58b377a290de39719
krish vaghasiya <kvaghasiya705@gmail.com>
2025-05-12 18:47:13 +0530

    Solved issues

--- src/components/homecomponents/ltsblogsection/index.js
+++ src/components/homecomponents/ltsblogsection/index.js
@@ -8,26 +8,30 @@ import Blogimage3 from "@/assets/image/blogimage3.jpg";
 import Arrow from "@/assets/icon/arrow";
 
 export default function Ltsblogsection() {
-    const Blogdata = [
-        {
-            Blogimage: Blogimage1,
-            blogdate: "Mar 13, 2025",
-            blogtitle: "What is Backtesting and How Do You Backtest a Trading Strategy?",
-            blogdetail: "Backtesting evaluates a trading strategy using historical data to assess performance before applying it to live markets...."
-        },
-        {
-            Blogimage: Blogimage2,
-            blogdate: "Mar 13, 2025",
-            blogtitle: "NSE Holiday List 2025",
-            blogdetail: "NSE holidays are designated non-trading days when the National Stock Exchange remains closed. These include national holidays, festivals..."
-        },
-        {
-            Blogimage: Blogimage3,
-            blogdate: "Mar 13, 2025",
-            blogtitle: "What is a Stockbroker? A Complete Guide",
-            blogdetail: "A stockbroker is a licensed professional who buys and sells stocks on behalf of clients, providing market insights and advice...."
-        }
-    ]
+  const Blogdata = [
+    {
+      Blogimage: Blogimage1,
+      blogdate: "Mar 13, 2025",
+      blogtitle:
+        "What is Backtesting and How Do You Backtest a Trading Strategy?",
+      blogdetail:
+        "Backtesting evaluates a trading strategy using historical data to assess performance before applying it to live markets....",
+    },
+    {
+      Blogimage: Blogimage2,
+      blogdate: "Mar 13, 2025",
+      blogtitle: "NSE Holiday List 2025",
+      blogdetail:
+        "NSE holidays are designated non-trading days when the National Stock Exchange remains closed. These include national holidays, festivals...",
+    },
+    {
+      Blogimage: Blogimage3,
+      blogdate: "Mar 13, 2025",
+      blogtitle: "What is a Stockbroker? A Complete Guide",
+      blogdetail:
+        "A stockbroker is a licensed professional who buys and sells stocks on behalf of clients, providing market insights and advice....",
+    },
+  ];
   return (
     <div className={styles.ltsblogsectionmain}>
       <div className="container">
@@ -41,33 +45,33 @@ export default function Ltsblogsection() {
           </p>
         </div>
         <div className={styles.ltsblogcardgrd}>
-            {Blogdata.map((i) => (
-                <div className={styles.ltsblogcardmain}>
-                    <div className={styles.blogimage}>
-                    <Image
-                        src={i.Blogimage}
-                        alt="Blogimage"
-                        width={1000}
-                        height={1000}
-                    />
-                    </div>
-                    <div className={styles.blogdetails}>
-                    <div className={styles.blogdateandbutton}>
-                        <button type="button" className={styles.sharemarketbutton}>
-                        <span>Share Market</span>
-                        </button>
-                        <span className={styles.blogcarddate}>{i.blogdate}</span>
-                    </div>
-                    <h5>
-                        {i.blogtitle}
-                    </h5>
-                    <p>
-                        {i.blogdetail}
-                    </p>
-                    <span className={styles.bloglink}>Read More <Arrow /></span>
-                    </div>
+          {Blogdata.map((i) => (
+            <div className={styles.ltsblogcardmain}>
+              <div className={styles.blogimage}>
+                <Image
+                  src={i.Blogimage}
+                  alt="Blogimage"
+                  width={1000}
+                  height={1000}
+                />
+              </div>
+              <div className={styles.blogdetails}>
+                <div>
+                  <div className={styles.blogdateandbutton}>
+                    <button type="button" className={styles.sharemarketbutton}>
+                      <span>Share Market</span>
+                    </button>
+                    <span className={styles.blogcarddate}>{i.blogdate}</span>
+                  </div>
+                  <h5>{i.blogtitle}</h5>
+                  <p>{i.blogdetail}</p>
                 </div>
-            ))}
+                <span className={styles.bloglink}>
+                  Read More <Arrow />
+                </span>
+              </div>
+            </div>
+          ))}
         </div>
         <div className={styles.ltsblogviewbutton}>
           <CommonButton
